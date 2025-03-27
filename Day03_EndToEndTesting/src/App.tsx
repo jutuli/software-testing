e0b8d8f2e0d6362a8ad64a06db31e0f9245b0669
Julia T <juliaturkali@gmail.com>
2025-03-27 12:34:23 +0100

    Day 3 - Initial Commit

---
+++ Day03_EndToEndTesting/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import NewsletterForm from './components/NewsletterForm';
+
+function App() {
+  return (
+    <>
+      <NewsletterForm />
+    </>
+  );
+}
+
+export default App;
